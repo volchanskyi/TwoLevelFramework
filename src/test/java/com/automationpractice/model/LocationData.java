5fd48faa49754af17635c9896bcadbaa7dcc9e8c
volchanskyi <ivan.volchanskyi@gmail.com>
2019-06-19 22:40:28 -0700

    add Simple XML serializer framework

--- src/test/java/com/automationpractice/model/LocationData.java
+++ src/test/java/com/automationpractice/model/LocationData.java
@@ -1,61 +1,37 @@
 package com.automationpractice.model;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
 
-@XmlRootElement(name = "CityStateLookupResponse")
-//@XmlType( propOrder = { "name", "city", "permanent", "special" } )
+@Root(name = "CityStateLookupResponse")
+
 public class LocationData {
+	@Element(name = "State")
 	private String state;
+	@Element(name = "ZipCode", required = false)
 	private String zipCode;
+	@Attribute(name = "ID", required = false)
 	private String id;
-	private String Zip5;
+	@Element(name = "Zip5")
+	private String postalCode;
+	@Element(name = "City")
 	private String city;
 
 	public String getZipCode() {
 		return this.zipCode;
 	}
 
-	@XmlElement(name = "ZipCode")
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public String getId() {
-		return this.id;
-	}
-
-	@XmlAttribute(name = "ID")
-	public void setId(String id) {
-		this.id = id;
-	}
-
 	public String getZip() {
-		return this.Zip5;
+		return this.postalCode;
 	}
 
-	@XmlElement(name = "Zip5")
-	public void setZip(String Zip5) {
-		this.Zip5 = Zip5;
-	}
-
-	@XmlElement(name = "City")
 	public String getCity() {
 		return this.city;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	@XmlElement(name = "State")
 	public String getState() {
 		return this.state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
 	}
 
 }
--- src/test/java/com/automationpractice/tests/TestDataObjectGeneratorHelper.java
+++ src/test/java/com/automationpractice/tests/TestDataObjectGeneratorHelper.java
@@ -9,6 +9,8 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 import org.openqa.selenium.json.JsonException;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -103,46 +105,26 @@ public class TestDataObjectGeneratorHelper {
 
 	// Location Data Helper
 	protected static String[] getLocationData() {
-//		InputStream xml = null;
-		String xml = null;
 		try {
-			xml = Request.Post("http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup")
+			InputStream xml = Request.Post("http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup")
 					.addHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9")
 					.bodyForm(Form.form().add("API", "CityStateLookup")
 							.add("XML", "<CityStateLookupRequest USERID=\"559REMOT6381\"><ZipCode ID=\"0\"><Zip5>"
 									+ generateValidFormatPostalCode() + "</Zip5></ZipCode></CityStateLookupRequest>")
 							.build())
-					.execute().returnContent().asString();
+					.execute().returnContent().asStream();
+			Serializer serializer = new Persister();
+			LocationData parsedXml = serializer.read(LocationData.class, xml);
+			String zip = parsedXml.getZip();
+			String city = parsedXml.getCity();
+			String state = parsedXml.getState();
+			String[] locationData = { zip, city, state };
+			return locationData;
 		} catch (IOException e) {
 			DATA_GEN_LOGGER.error(e.toString());
-		}
-		// getting the xml file to read
-		try {
-			File xmlFile = new File("src/test/resources/test.xml");
-			JAXBContext jaxbContext = JAXBContext.newInstance(LocationData.class);
-			LocationData parsedXml = (LocationData) jaxbContext.createUnmarshaller().unmarshal(xmlFile);
-			String zip = parsedXml.getZip();
-//			String city = parsedXml.getCity();
-//			String state = parsedXml.getState();
-//			String[] locationData = { zip, city, state };
-//			return locationData;
-			return null;
-		} catch (JAXBException e) {
+		} catch (Exception e) {
 			DATA_GEN_LOGGER.error(e.toString());
 		}
-//		} catch (JAXBException e) {
-//			DATA_GEN_LOGGER.error(e.toString());
-//		
-//		// checking return status
-//		if (status.equals("SUCCESS")) {
-//			String zip = parsed.getAsJsonObject().get("zip5").getAsString();
-//			String city = parsed.getAsJsonObject().get("defaultCity").getAsString();
-//			String state = parsed.getAsJsonObject().get("defaultState").getAsString();
-//			String[] locationData = { zip, city, state };
-//			return locationData;
-//		} else {
-//			throw new JsonException("api request error" + status);
-//		}
 		return null;
 	}
 
