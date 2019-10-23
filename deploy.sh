86ab964a5981076c772580eb08ebbf03f6639fa9
volchanskyi <ivan.volchanskyi@gmail.com>
2019-10-22 21:13:17 -0700

    ref

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 #docker rm nexus > "/dev/null 2>&1"
 # JOB_NAME is the name of the project of this build. This is the name you gave your job. It is set up by Jenkins
 COMPOSE_ID=${JOB_NAME:-local}
+
+echo $COMPOSE_ID
 # Remove Previous Stack
 docker-compose -p $COMPOSE_ID rm -f
 # Starting new stack environment
